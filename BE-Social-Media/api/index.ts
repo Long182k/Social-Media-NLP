837f0df8ab68abaec941c2d6dc8903d539448758
Long Nguyen <thanhlong9123@gmail.com>
2026-07-26 00:48:58 +0700

    fix(be): path mapping for prisma client and import compiled AppModule from dist

--- BE-Social-Media/api/index.ts
+++ BE-Social-Media/api/index.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../src/app.module';
+import { AppModule } from '../dist/src/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import express from 'express';
